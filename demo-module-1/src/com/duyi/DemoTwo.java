d605cf055506006a3af2dc002eb171acecf54b1c
Administrator <976830634@qq.com>
2022-03-15 15:51:57 +0800

    第三次尝试

--- demo-module-1/src/com/duyi/DemoTwo.java
+++ demo-module-1/src/com/duyi/DemoTwo.java
@@ -1,4 +1,9 @@
 package com.duyi;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class DemoTwo {
+    public static void main(String[] args) {
+        System.out.println(666);
+    }
 }
